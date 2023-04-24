da57db5a4b9c491aa8631a1c27aab5f241f0da71
AckimJnr <acklongwe@gmail.com>
2023-04-25 01:24:37 +0200

    Update printc documentantion

--- printc.c
+++ printc.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
   * printc - prints character
-  * @num: value entered
+  * @c: value entered
   *
   * Return: Returns number of chars printed
   */
 int printc(int c)
 {
 	int ret = (write(STDOUT_FILENO, &c, 1));
+
 	if (ret == -1)
 		perror("write");
 	return (ret);
