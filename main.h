e8e083e9a61025afd68b8beb56ab7f509b51e413
Reh05 <rehemachiona05@gmail.com>
2023-04-26 01:48:53 +0200

    Update INT_MIN handler

--- main.h
+++ main.h
@@ -4,6 +4,6 @@
 int _printf(const char *format, ...);
 int printc(const int c);
 int print_string(const char *str);
-int print_num(long int num);
+int print_num(int n);
 
 #endif
--- print_num.c
+++ print_num.c
@@ -3,18 +3,25 @@
 #include "main.h"
 /**
  * print_num - prints a given number
- * @num: number to be printed
+ * @n: number to be printed
  *
  * Return: Returns the number of numbers printed
  */
-int print_num(long int num)
+int print_num(int n)
 {
 	char str[20];
 	int i = 0;
 	int count = 0;
 	char c = '-';
+	long int num = n;
 
-	if (num < 0)
+	if (num == -2147483648)
+	{
+		write(STDOUT_FILENO, &c, 1);
+		count++;
+		num = -num;
+	}
+	else if (num < 0)
 	{
 		write(1, &c, 1);
 		count++;
